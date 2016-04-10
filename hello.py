a0ae91e97d43c855ae0e52adaea49977be953641
KK <741077081@qq.com>
2016-04-10 16:45:03 +0800

    change

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask.ext.script import Manager
 App=Flask(__name__)
 manager=Manager(App)
 @App.route('/')
 def index():
-    return '<h1>hello world!</h1>'
+    return render_template('first.html')
 if __name__=='__main__':
     manager.run()
