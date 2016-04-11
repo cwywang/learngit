20c0e39c82cadb37deff2aa863da3c98fb4af387
KK <741077081@qq.com>
2016-04-11 22:26:25 +0800

    hello_windows

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 from flask import Flask,render_template
 from flask.ext.script import Manager
-App=Flask(__name__)
+App=Flask(__name__,static_url_path='')
 manager=Manager(App)
 @App.route('/')
 def first():
     return render_template('first.html')
 @App.route('/樊恩华')
 def index():
-    return render_template('index.html')
+    return app.send_static_file('index.html')
 if __name__=='__main__':
     manager.run()
