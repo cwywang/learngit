99a0232bfc0171c98639fe2cc1e826019b70e9fa
KK <741077081@qq.com>
2016-04-13 20:41:47 +0800

    hello_windows

--- hello.py
+++ hello.py
@@ -2,7 +2,6 @@ from flask import Flask,render_template,request
 from flask.ext.script import Manager
 import re
 App=Flask(__name__)
-manager=Manager(App)
 @App.route('/')
 def first():
     return render_template('first.html')
@@ -12,4 +11,4 @@ def time():
         return "<h1>暂时无法登录凡恩华的个人网站 </br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
     return render_template('time.html')
 if __name__=='__main__':
-	manager.run()
+	App.run()
