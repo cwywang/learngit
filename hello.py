33dc97a3d680a27d2d87be4054ea1ea2de0ee9f1
KK <741077081@qq.com>
2016-04-13 17:19:32 +0800

    hello_windows

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ def first():
 @App.route('/凡恩华')
 def time():
     if(re.match(".*iPhone.*",request.headers.get('User-Agent')) or re.match(".*Android.*",request.headers.get('User-Agent'))):
-        return "<h1>暂时无法登录凡恩华的个人网站</br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
+        return "<h1>暂时无法登录凡恩华的个人网站 </br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
     return render_template('time.html')
 if __name__=='__main__':
 	manager.run()
