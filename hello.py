5b2a19f8eb15f0a91c54aec64f60d0789a3f11b0
KK <741077081@qq.com>
2016-04-15 17:13:06 +0800

    hello_windows

--- hello.py
+++ hello.py
@@ -9,14 +9,14 @@ def first():
 def about():
     if(re.match(".*iPhone.*",request.headers.get('User-Agent')) or re.match(".*Android.*",request.headers.get('User-Agent'))):
         return "<h1>暂时无法登录凡恩华的个人网站 </br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
-    return render_template('about.html')
+    return render_template('樊恩华/about.html')
 @App.route('/樊恩华')
 def time():
     if(re.match(".*iPhone.*",request.headers.get('User-Agent')) or re.match(".*Android.*",request.headers.get('User-Agent'))):
         return "<h1>暂时无法登录凡恩华的个人网站 </br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
-    return render_template('time.html')
+    return render_template('樊恩华/time.html')
 @App.route('/刘鹏')
 def love():
-    return render_template('love.htm')
+    return render_template('刘鹏/love.htm')
 if __name__=='__main__':
 	App.run()
