719977798682adc95116c810b83febc20d3b7ccf
KK <741077081@qq.com>
2016-04-16 21:52:50 +0800

    hello_windows

--- hello.py
+++ hello.py
@@ -5,18 +5,18 @@ App=Flask(__name__)
 @App.route('/')
 def first():
     return render_template('first.html')
-@App.route('/about.html')
+@App.route('/樊恩华_Readmore')
 def about():
     if(re.match(".*iPhone.*",request.headers.get('User-Agent')) or re.match(".*Android.*",request.headers.get('User-Agent'))):
         return "<h1>暂时无法登录凡恩华的个人网站 </br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
-    return render_template('樊恩华/about.html')
+    return render_template('Fanenhua/about.html')
 @App.route('/樊恩华')
 def time():
     if(re.match(".*iPhone.*",request.headers.get('User-Agent')) or re.match(".*Android.*",request.headers.get('User-Agent'))):
         return "<h1>暂时无法登录凡恩华的个人网站 </br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
-    return render_template('樊恩华/time.html')
+    return render_template('Fanenhua/time.html')
 @App.route('/刘鹏')
 def love():
-    return render_template('刘鹏/love.htm')
+    return render_template('Liupeng/love.htm')
 if __name__=='__main__':
 	App.run()
