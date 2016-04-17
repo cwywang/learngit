f6b8c8d9cfd506a637e6c8c779c439cd1a3d7b70
KK <741077081@qq.com>
2016-04-17 20:23:32 +0800

    hello_windows

--- hello.py
+++ hello.py
@@ -10,7 +10,7 @@ def us():
     if(re.match(".*iPhone.*",request.headers.get('User-Agent')) or re.match(".*Android.*",request.headers.get('User-Agent'))):
         return "<h1>暂时无法登录凡恩华的个人网站 </br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
     return render_template('Fanenhua/us.html')
-@App.route('/樊恩华_Readmore')
+@App.route('/樊恩华_readmore')
 def about():
     if(re.match(".*iPhone.*",request.headers.get('User-Agent')) or re.match(".*Android.*",request.headers.get('User-Agent'))):
         return "<h1>暂时无法登录凡恩华的个人网站 </br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
@@ -25,8 +25,6 @@ def Liupeng():
     return render_template('Liupeng/Liupeng.html')
 @App.route('/贺浪')
 def Helang():
-    if(re.match(".*iPhone.*",request.headers.get('User-Agent')) or re.match(".*Android.*",request.headers.get('User-Agent'))):
-        return "<h1>暂时无法登录凡恩华的个人网站 </br></h1><h1>hadNone团队正在修复手机端，请登录电脑查看网页！</h1>"
     return render_template('Helang/Helang.html')
 if __name__=='__main__':
 	App.run()
