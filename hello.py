13923758110614459ad2f9a3057045335686b277
KK <741077081@qq.com>
2016-04-21 00:53:39 +0800

    hello_windows

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request,redirect,url_for
-from flask.ext.wtf import Form,Mail
-from flask.ext.mail import Message
+from flask.ext.wtf import Form
+from flask.ext.mail import Message,Mail
 import re
 import os
 App=Flask(__name__)
