8b6d3ec8855bc5344132a24be0d1e0babf8d931a
KK <741077081@qq.com>
2016-04-21 12:30:45 +0800

    hello_windows

--- hello.py
+++ hello.py
@@ -13,7 +13,7 @@ App.config['MAIL_PORT']=25
 App.config['MAIL_USE_TLS']=True
 App.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
 App.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
-mail=Mail(App)
+
 @App.route('/樊恩华_us')
 def us():
     if(re.match(".*iPhone.*",request.headers.get('User-Agent')) or re.match(".*Android.*",request.headers.get('User-Agent'))):
